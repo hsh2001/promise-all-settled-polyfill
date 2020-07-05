e1f7eead0b6c6b6501c5346f4994d4d9e0d39176
Seung-hyun Hwang <dev@hsh.kr>
2020-07-05 11:41:52 +0900

    Setup jest

--- __test__/index.test.ts
+++ __test__/index.test.ts
@@ -1,12 +1,19 @@
 import promiseAllSettled from '../index';
 
 describe('Test Promise.allSettled', () => {
+  it('should define method correctly', () => {
+    expect(promiseAllSettled === Promise.allSettled).toBeTruthy();
+  });
+
   it('should solve Promise list correvtly', async () => {
     const result = await promiseAllSettled([
       Promise.resolve(1),
-      Promise.reject(1),
+      Promise.reject(2),
     ]);
 
-    expect(result).toHaveLength(2);
+    expect(result).toStrictEqual([
+      { value: 1, status: 'fulfilled' },
+      { reason: 2, status: 'rejected' },
+    ]);
   });
 });
