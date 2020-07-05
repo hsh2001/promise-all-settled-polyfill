729d7cbd06f431be231d37044532362c369b28ce
Seung-hyun Hwang <dev@hsh.kr>
2020-07-05 11:41:45 +0900

    Update index.ts
    Create Promise.allSettled polyfill

--- index.ts
+++ index.ts
@@ -1,5 +1,37 @@
-function promiseAllSettled(promises: Promise<any>[]): Promise<any> {
-  return Promise.reject('This is for test.');
+interface PomiseAllSettledResult<T> {
+  status: 'fulfilled' | 'rejected';
+  value?: T;
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  reason?: Error | any;
+}
+
+function promiseAllSettled<T>(
+  promises: Promise<T>[],
+): Promise<PomiseAllSettledResult<T>[]> {
+  return Promise.all(
+    promises.map((promise) => {
+      return Promise.resolve()
+        .then(() => promise)
+        .then(
+          (value): PomiseAllSettledResult<T> => ({
+            value,
+            status: 'fulfilled',
+          }),
+        )
+        .catch(
+          (reason): PomiseAllSettledResult<T> => ({
+            reason,
+            status: 'rejected',
+          }),
+        );
+    }),
+  );
+}
+
+declare global {
+  interface PromiseConstructor {
+    allSettled: typeof promiseAllSettled;
+  }
 }
 
 Promise.allSettled = promiseAllSettled;
